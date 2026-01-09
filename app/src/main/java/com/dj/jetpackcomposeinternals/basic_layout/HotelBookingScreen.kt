cfef3f336080c85d8c41fe77198905348b26e731
DJDrama <putzzang@naver.com>
2026-01-09 18:55:52 +0900

    8. Practice - Building a Responsive UI With Figma Mockups

---
+++ app/src/main/java/com/dj/jetpackcomposeinternals/basic_layout/HotelBookingScreen.kt
@@ -0,0 +1,313 @@
+package com.dj.jetpackcomposeinternals.basic_layout
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.window.core.layout.WindowWidthSizeClass
+import com.dj.jetpackcomposeinternals.R
+import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
+
+private val tags = listOf(
+    "City Center",
+    "Luxury",
+    "Instant Booking",
+    "Exclusive Deals",
+    "Early Bird Discount",
+    "Romantic Gateway",
+    "24/7 Support",
+)
+
+private val offers = mapOf(
+    R.drawable.bed to "2 Bed",
+    R.drawable.breakfast to "Breakfast",
+    R.drawable.cutlery to "Cutlery",
+    R.drawable.pawprint to "Pet Friendly",
+    R.drawable.serving_dish to "Air Conditioning",
+    R.drawable.snowflake to "Air Conditioning",
+    R.drawable.television to "TV",
+    R.drawable.wi_fi_icon to "Wifi",
+
+    )
+
+@Composable
+fun HotelBookingScreen(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        verticalArrangement = Arrangement.spacedBy(space = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Box {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(max = 250.dp),
+                    contentScale = ContentScale.Crop,
+                    painter = painterResource(R.drawable.living_room),
+                    contentDescription = null
+                )
+                HotelFadedBanner(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(alignment = Alignment.BottomCenter)
+                )
+            }
+        }
+        item {
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            )
+        }
+        item {
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
+            ) {
+                tags.forEach { tag ->
+                    AssistChip(onClick = {}, label = {
+                        Text(text = tag)
+                    })
+                }
+            }
+        }
+        item {
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            )
+        }
+        item {
+            Text(
+                text = """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                """.trimIndent(),
+                fontSize = 13.sp,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+        }
+        item {
+            Text(
+                text = "What we offer",
+                fontSize = 13.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+
+        item {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
+            ) {
+                items(items = offers.entries.toList()) { (drawableResId, label) ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .background(
+                                color = Color.Gray
+                                    .copy(0.3f)
+                            )
+                            .clip(RoundedCornerShape(size = 12.dp))
+                            .padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(drawableResId),
+                            contentDescription = label,
+                            modifier = Modifier.size(36.dp)
+                        )
+                        Text(
+                            text = label,
+                            fontSize = 13.sp
+                        )
+                    }
+                }
+            }
+        }
+        item {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .padding(16.dp)
+                    .widthIn(
+                        //min = 300.dp,
+                        max = 400.dp
+                    )
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Book now!")
+            }
+        }
+    }
+}
+
+@Composable
+fun HotelFadedBanner(modifier: Modifier = Modifier) {
+    val windowClass = currentWindowAdaptiveInfo().windowSizeClass
+    Row(
+        modifier = modifier
+            .background(Color.White.copy(alpha = 0.6f))
+            .padding(all = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(space = 4.dp)
+        ) {
+            Text(
+                text = "Hotel California Strawberry",
+                fontWeight = FontWeight.Bold,
+                fontSize = when (windowClass.windowWidthSizeClass) {
+                    WindowWidthSizeClass.COMPACT -> 18.sp
+                    WindowWidthSizeClass.MEDIUM -> 24.sp
+                    WindowWidthSizeClass.EXPANDED -> 28.sp
+                    else -> 18.sp
+                },
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            LabeledIcon(
+                text = "Los Angeles, California",
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.LocationOn,
+                        tint = Color.DarkGray,
+                        contentDescription = null
+                    )
+                })
+            LabeledIcon(
+                text = "4.9 (13k reviews)",
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        tint = Color.Yellow,
+                        contentDescription = null
+                    )
+                })
+        }
+        Text(
+            text = buildAnnotatedString {
+                val fontSizeMultiplier = when (windowClass.windowWidthSizeClass) {
+                    WindowWidthSizeClass.COMPACT -> 1f
+                    WindowWidthSizeClass.MEDIUM -> 1.2f
+                    WindowWidthSizeClass.EXPANDED -> 1.5f
+                    else -> 1f
+                }
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp * fontSizeMultiplier
+                    )
+                ) {
+                    append("420$/")
+                }
+                withStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp * fontSizeMultiplier
+                    )
+                ) {
+                    append("night")
+                }
+            }
+        )
+
+    }
+}
+
+@Composable
+fun LabeledIcon(
+    text: String,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(space = 4.dp)
+    ) {
+        icon()
+        Text(
+            text = text,
+            fontSize = 13.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun HotelBookingScreenPreview() {
+    JetpackComposeInternalsTheme {
+        HotelBookingScreen()
+    }
+
+}
+
+
+@Preview(
+    device = Devices.NEXUS_10
+)
+@Composable
+private fun HotelBookingScreenTabletPreview() {
+    JetpackComposeInternalsTheme {
+        HotelBookingScreen()
+    }
+
+}
