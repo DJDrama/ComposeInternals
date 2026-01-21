b6464c0be888592e7a33d3449aa4dca02435ced3
DJDrama <putzzang@naver.com>
2026-01-21 16:13:45 +0900

    3-6 Focus Management

--- app/src/main/java/com/dj/jetpackcomposeinternals/MainActivity.kt
+++ app/src/main/java/com/dj/jetpackcomposeinternals/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeGestures
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.dj.jetpackcomposeinternals.basic_modifiers.SpacingModifierDemo
+import com.dj.jetpackcomposeinternals.basic_modifiers.FocusManagementModifiers
 import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,19 +22,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     contentWindowInsets = WindowInsets.safeGestures
                 ) { innerPadding ->
-                    SpacingModifierDemo(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .consumeWindowInsets(
-                                innerPadding
-                            )
+                    FocusManagementModifiers()
+//                    SpacingModifierDemo(
+//                        modifier = Modifier
+//                            .padding(innerPadding)
+//                            .consumeWindowInsets(
+//                                innerPadding
+//                            )
 //                        modifier = Modifier
 //                            .statusBarsPadding()
 //                            .navigationBarsPadding()
 //                            .safeGesturesPadding()
 //                            .safeDrawingPadding()
 //                            .safeContentPadding()
-                    )
+                    //)
                 }
             }
         }
