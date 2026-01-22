0917fc08645643a241d1e16379682b8c710dab15
DJDrama <putzzang@naver.com>
2026-01-22 17:37:52 +0900

    4-5 Custom Layouts

---
+++ app/src/main/java/com/dj/jetpackcomposeinternals/measurements/PagedRow.kt
@@ -0,0 +1,95 @@
+package com.dj.jetpackcomposeinternals.measurements
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
+
+@Composable
+fun PagedRow(
+    page: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Layout(
+        content = content,
+        modifier = modifier,
+        measurePolicy = { measurables, constraints ->
+            val placeable = measurables.map {
+                it.measure(constraints)
+            }
+            val pages = mutableListOf<List<Placeable>>()
+            var currentPage = mutableListOf<Placeable>()
+            var currentPageWidth = 0
+
+            placeable.fastForEach { placeable ->
+                if (currentPageWidth + placeable.width > constraints.maxWidth) {
+                    pages.add(currentPage)
+                    currentPage = mutableListOf()
+                    currentPageWidth = 0
+                }
+                currentPage.add(placeable)
+                currentPageWidth += placeable.width
+            }
+
+            if (currentPage.isNotEmpty()) {
+                pages.add(currentPage)
+            }
+
+            val pageItems = pages.getOrNull(index = page) ?: emptyList()
+            // val maxHeight = pageItems.fastMaxOfOrNull { it.height } ?: 0
+
+            layout(width = constraints.maxWidth, height = constraints.maxHeight) {
+                var xOffset = 0
+                pageItems.forEach { placeable ->
+                    placeable.place(xOffset, 0)
+                    xOffset += placeable.width
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun PagedRowPreview() {
+    JetpackComposeInternalsTheme {
+        PagedRow(
+            page = 0
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(100.dp)
+                    .background(Color.Red)
+            )
+            Box(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(150.dp)
+                    .background(Color.Yellow)
+            )
+            Box(
+                modifier = Modifier
+                    .width(25.dp)
+                    .height(120.dp)
+                    .background(Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(100.dp)
+                    .background(Color.Blue)
+            )
+        }
+    }
+}
