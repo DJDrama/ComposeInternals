718c4afdb4cea39ea9c43d9442329f8c8ffd9756
DJDrama <putzzang@naver.com>
2026-01-22 17:56:35 +0900

    4-6 Subcompose Layouts

--- app/src/main/java/com/dj/jetpackcomposeinternals/measurements/SubcomposePagedRow.kt
+++ app/src/main/java/com/dj/jetpackcomposeinternals/measurements/SubcomposePagedRow.kt
@@ -7,64 +7,66 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastMaxOfOrNull
 import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
 
 @Composable
-fun PagedRow(
+fun SubcomposePagedRow(
     page: Int,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
-    Layout(
-        content = content,
+    SubcomposeLayout(
         modifier = modifier,
-        measurePolicy = { measurables, constraints ->
-            val placeable = measurables.map {
-                it.measure(constraints)
-            }
-            val pages = mutableListOf<List<Placeable>>()
-            var currentPage = mutableListOf<Placeable>()
-            var currentPageWidth = 0
+    ) { constraints ->
+        val pages = mutableListOf<List<Placeable>>()
+        var currentPage = mutableListOf<Placeable>()
+        var currentPageWidth = 0
 
-            placeable.fastForEach { placeable ->
-                if (currentPageWidth + placeable.width > constraints.maxWidth) {
-                    pages.add(currentPage)
-                    currentPage = mutableListOf()
-                    currentPageWidth = 0
+        val measurables = subcompose("content", content)
+        var i = 0
+        for (measurable in measurables) {
+            val placeable = measurable.measure(constraints.copy(minWidth = 0, minHeight = 0))
+            i++
+            if (currentPageWidth + placeable.width > constraints.maxWidth) {
+                if (pages.size == page) {
+                    break
                 }
-                currentPage.add(placeable)
-                currentPageWidth += placeable.width
-            }
-
-            if (currentPage.isNotEmpty()) {
                 pages.add(currentPage)
+                currentPage = mutableListOf()
+                currentPageWidth = 0
             }
+            currentPage.add(placeable)
+            currentPageWidth += placeable.width
+        }
+        println("We measured $i composables.")
 
-            val pageItems = pages.getOrNull(index = page) ?: emptyList()
-            val maxHeight = pageItems.fastMaxOfOrNull { it.height } ?: 0
+        if (currentPage.isNotEmpty()) {
+            pages.add(currentPage)
+        }
 
-            layout(width = constraints.maxWidth, height = maxHeight) {
-                var xOffset = 0
-                pageItems.forEach { placeable ->
-                    placeable.place(xOffset, 0)
-                    xOffset += placeable.width
-                }
+        val pageItems = pages.getOrNull(index = page) ?: emptyList()
+        val maxHeight = pageItems.fastMaxOfOrNull { it.height } ?: 0
+
+        layout(width = constraints.maxWidth, height = maxHeight) {
+            var xOffset = 0
+            pageItems.forEach { placeable ->
+                placeable.place(xOffset, 0)
+                xOffset += placeable.width
             }
         }
-    )
+    }
 }
 
 @Preview
 @Composable
-private fun PagedRowPreview() {
+private fun SubcomposePagedRowPreview() {
     JetpackComposeInternalsTheme {
-        PagedRow(
+        SubcomposePagedRow(
             page = 0
         ) {
             Box(
