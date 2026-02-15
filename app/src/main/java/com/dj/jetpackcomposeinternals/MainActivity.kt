db4c2744b33cd864b7c914f2f1707d51e04d3469
DJDrama <putzzang@naver.com>
2026-02-15 17:33:04 +0900

    4-7 Custom Lazy Mind Map Layout

--- app/src/main/java/com/dj/jetpackcomposeinternals/MainActivity.kt
+++ app/src/main/java/com/dj/jetpackcomposeinternals/MainActivity.kt
@@ -4,26 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.dj.jetpackcomposeinternals.measurements.SubcomposePagedRow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.IntOffset
+import com.dj.jetpackcomposeinternals.measurements.LazyMindMap
+import com.dj.jetpackcomposeinternals.measurements.MindMapItem
 import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,30 +27,64 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) { innerPadding ->
-                    var page by remember {
-                        mutableIntStateOf(0)
+                    val mindMapItems = remember {
+                        listOf(
+                            MindMapItem(
+                                title = "Hello World 1",
+                                percentageOffset = Offset(0f, 0f)
+                            ),
+                            MindMapItem(
+                                title = "Hello World 2",
+                                percentageOffset = Offset(1f, 0f)
+                            ),
+                            MindMapItem(
+                                title = "Hello World 3",
+                                percentageOffset = Offset(0.3f, -0.5f)
+                            ),
+                            MindMapItem(
+                                title = "Hello World 4",
+                                percentageOffset = Offset(-0.2f, 1.5f)
+                            ),
+
+                            )
                     }
-                    Column(
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        SubcomposePagedRow(
-                            page = 0,
-                        ) {
-                            (1..1000).forEach {
-                                Box(
-                                    modifier = Modifier
-                                        .height(100.dp)
-                                        .width(Random.nextInt(300).dp)
-                                        .background(Color(Random.nextInt()))
-                                )
-                            }
-                        }
-                        Button(onClick = {
-                            page++
-                        }) {
-                            Text("Go to next page")
-                        }
+                    var mindMapOffset by remember {
+                        mutableStateOf(IntOffset.Zero)
                     }
+                    LazyMindMap(
+                        items = mindMapItems,
+                        mindMapOffset = mindMapOffset,
+                        onDrag = { delta ->
+                            mindMapOffset += delta
+                        },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
+                    /* var page by remember {
+                         mutableIntStateOf(0)
+                     }
+                     Column(
+                         modifier = Modifier.padding(innerPadding)
+                     ) {
+                         SubcomposePagedRow(
+                             page = 0,
+                         ) {
+                             (1..1000).forEach {
+                                 Box(
+                                     modifier = Modifier
+                                         .height(100.dp)
+                                         .width(Random.nextInt(300).dp)
+                                         .background(Color(Random.nextInt()))
+                                 )
+                             }
+                         }
+                         Button(onClick = {
+                             page++
+                         }) {
+                             Text("Go to next page")
+                         }
+                     }*/
 
 //                    SpacingModifierDemo(
 //                        modifier = Modifier
