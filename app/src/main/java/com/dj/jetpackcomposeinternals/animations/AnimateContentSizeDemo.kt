00f47d37bf746392f00f56efac7d241d91869736
DJDrama <putzzang@naver.com>
2026-02-24 11:46:13 +0900

    9.3 Animating Size Changes

---
+++ app/src/main/java/com/dj/jetpackcomposeinternals/animations/AnimateContentSizeDemo.kt
@@ -0,0 +1,57 @@
+package com.dj.jetpackcomposeinternals.animations
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
+
+@Composable
+fun AnimateContentSizeDemo(modifier: Modifier = Modifier) {
+    var toggle by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            toggle = !toggle
+        }) {
+            Text(text = "Toggle")
+        }
+
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+                .animateContentSize() // must place before size change
+                .height(if (toggle) 400.dp else 200.dp)
+                .fillMaxWidth()
+        ) {
+
+        }
+        Text(text = "I'm below")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AnimateContentSizeDemoPreview() {
+    JetpackComposeInternalsTheme {
+        AnimateContentSizeDemo()
+    }
+
+}
