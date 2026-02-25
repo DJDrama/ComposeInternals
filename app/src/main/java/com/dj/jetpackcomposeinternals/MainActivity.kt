f1e57855cf340f698b6a5e6b7cbec836fc9e4828
DJDrama <putzzang@naver.com>
2026-02-25 14:43:36 +0900

    10-3 Optimizing Lazy List Performance

--- app/src/main/java/com/dj/jetpackcomposeinternals/MainActivity.kt
+++ app/src/main/java/com/dj/jetpackcomposeinternals/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.dj.jetpackcomposeinternals.performance.ImageLoading
+import com.dj.jetpackcomposeinternals.performance.LazyListPerformance
 import com.dj.jetpackcomposeinternals.ui.theme.JetpackComposeInternalsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) { innerPadding ->
-                    ImageLoading(
+                    LazyListPerformance(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding))
